9d462c40174247a0cd6d99bfb89c7ac0f8d9293c
dheoprasetyo <dheoprasetyo.dp@gmail.com>
2019-07-13 00:05:06 +0700

    Animation Splash

--- app/src/main/java/com/example/catatankeuangan/Splash.java
+++ app/src/main/java/com/example/catatankeuangan/Splash.java
@@ -4,13 +4,33 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Splash extends AppCompatActivity {
 
+    Animation app_splash,btt;
+    ImageView app_logo;
+    TextView app_title;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        //load animation
+        app_splash = AnimationUtils.loadAnimation(this, R.anim.app_splash);
+        btt = AnimationUtils.loadAnimation(this, R.anim.btt);
+
+        //load elemen
+        app_logo = findViewById(R.id.app_logo);
+        app_title = findViewById(R.id.app_title);
+
+        //run animation
+        app_logo.startAnimation(app_splash);
+        app_title.startAnimation(btt);
+
 
         Handler handler = new Handler();
             handler.postDelayed(new Runnable() {
