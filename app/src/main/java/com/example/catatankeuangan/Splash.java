cca6aaaff9d160f49704d9538eaa9128fb8c484e
sherientep <sherientrisnawatyep@gmail.com>
2019-07-12 15:29:20 +0700

    First Commit

--- app/src/main/java/com/example/catatankeuangan/Splash.java
+++ app/src/main/java/com/example/catatankeuangan/Splash.java
@@ -1,5 +1,7 @@
 package com.example.catatankeuangan;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,6 +12,14 @@ public class Splash extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-
+        Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent gotoutama = new Intent(Splash.this, MainActivity.class);
+                    startActivity(gotoutama);
+                    finish();
+                }
+                }, 2000);
     }
 }
